b1193d8dd7fb793571c185d523022e49373f16ff
Minami Kato <minamikato@gmail.com>
2018-04-23 19:25:33 -0400

    lecture note added

---
+++ 01 - New Variables/lecturenote.md
@@ -0,0 +1,8 @@
+
+var : works only inside of the function. cannot call from outside  //function scoping
+
+{} : block
+
+const: cannot be updated
+
+let : can be updated
---
+++ 02 - Arrow functions/lecturenote.js
@@ -0,0 +1,21 @@
+
+const names = ['wes', 'kait', 'lux'];
+
+const fullName = names.map(funciton(name){
+  return `${name} bos`;
+});
+
+const fullName2 = names.map( (name) => {
+  return `${name} bos`;
+});
+
+//かっこ省略可能 if  only 1 arg
+const fullName3 = names.map( name => {
+  return `${name} bos`;
+});
+
+//explicit return
+const fullName4 = names.map( name => {`${name} bos`});
+
+// no arguments
+const fullName5 = names.map( () => { `cool bos`});
